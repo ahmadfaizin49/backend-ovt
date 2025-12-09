7adc6f604ff0cbea4044529fd2cadd6f67d7836a
Ahmad Faizin <afaizin49@example.com>
2025-12-09 21:34:32 +0700

    fix overtime

--- src/helper/overtime.js
+++ src/helper/overtime.js
@@ -1,4 +1,4 @@
-function calculateOvertimeAmount(status, hours, basicSalary) {
+function calculateOvertimeAmount(status, hours, basicSalary, workingDays) {
     const hourlyWage = basicSalary / 173;
     let total = 0;
     if (status === 'NORMAL') {
@@ -13,8 +13,32 @@ function calculateOvertimeAmount(status, hours, basicSalary) {
             const totalFirtsHour = hourlyWage * 1.5;
             total = totalFirtsHour + (hourlyWage * remainingHours * 2);
         }
-    } else if (status === 'HOLIDAY') {
-        total = hourlyWage * hours * 2;
+    } else if (status === 'HOLIDAY' && workingDays === 5) {
+        if (hours <= 8) {
+            total = hourlyWage * hours * 2;
+        } else if (hours == 9) {
+            const firstEightHours = hourlyWage * 8 * 2;
+            const ninthHour = hourlyWage * 3;
+            total = firstEightHours + ninthHour;
+        } else if (hours > 9) {
+            const firstEightHours = hourlyWage * 8 * 2;
+            const ninthHour = hourlyWage * 3;
+            const remainingHours = hours - 9;
+            total = firstEightHours + ninthHour + (hourlyWage * remainingHours * 4);
+        }
+    } else if (status === 'HOLIDAY' && workingDays === 6) {
+        if (hours <= 7) {
+            total = hourlyWage * hours * 2;
+        } else if (hours == 8) {
+            const firstSevenHours = hourlyWage * 7 * 2;
+            const eighthHour = hourlyWage * 3;
+            total = firstSevenHours + eighthHour;
+        } else if (hours > 8) {
+            const firstSevenHours = hourlyWage * 7 * 2;
+            const eighthHour = hourlyWage * 3;
+            const remainingHours = hours - 8;
+            total = firstSevenHours + eighthHour + (hourlyWage * remainingHours * 4);
+        }
     }
     return total;
 }
